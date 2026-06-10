b623e58d8516349b9d8f3421a2922915af82702a
Parth Agarwal <agarwal.parth.101@gmail.com>
2026-06-10 18:12:19 +0530

    fix(script): Update set_node_mem.sh to work in MacOS

--- set_node_mem.sh
+++ set_node_mem.sh
@@ -1,5 +1,14 @@
-memory=$(awk '/MemTotal/{print $2}' /proc/meminfo);
-memory=$((memory / 1024));
-node_options="--max-old-space-size=$((memory - 1024))";
-echo "NODE_OPTIONS=$node_options";
-export NODE_OPTIONS="$node_options";
+#!/bin/bash
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    memory=$(sysctl -n hw.memsize)
+    memory=$((memory / 1024 / 1024))
+else
+    memory=$(awk '/MemTotal/{print $2}' /proc/meminfo)
+    memory=$((memory / 1024))
+fi
+
+node_options="--max-old-space-size=$((memory - 1024))"
+
+echo "NODE_OPTIONS=$node_options"
+export NODE_OPTIONS="$node_options"
